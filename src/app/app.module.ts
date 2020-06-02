d0ed75dce5a7853b58f5fc67a669dc839d33c7fd
Indhu <indhushivanip2001@gmail.com>
2020-06-02 20:21:01 +0530

    added src folder

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoot: Routes = [{path:'home', component: HomeComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoot)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
