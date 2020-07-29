07803f0e19a7fdd6140e1c33affc4974567591c7
Harshitha_Diddi <harshitha1579@gmail.com>
2020-07-29 13:19:01 +0530

    Added countdown

--- src/app/showevent/showevent.component.ts
+++ src/app/showevent/showevent.component.ts
@@ -8,11 +8,23 @@ import { Router } from '@angular/router';
 })
 export class ShoweventComponent implements OnInit {
   event : any;
+  eventdate: any;
+  s:String;
+  text:String;
+  d:String;
   constructor(private router: Router) { }
-
+  
   ngOnInit(): void {
     this.event = JSON.parse(localStorage.getItem('eventDetails'));
-    console.log(this.event);
+    this.eventdate = new Date(this.event.eventDate);
+    this.s = new String(this.eventdate);
+    this.text=this.s.slice(3,-31);
+    var b = ",";
+var position = 4;
+var output = [this.text.slice(0,4), b, this.text.slice(position)].join('');
+console.log(output);
+    localStorage.setItem("EventDate",this.eventdate);
+       console.log(this.event);
   }
   
   payment() {
