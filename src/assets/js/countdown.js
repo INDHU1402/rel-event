a5b5227a4a80fbccf2c74cffaa14c1968f3af6e2
IndhuShivani <18wh1a0446@bvrithyderabad.edu.in>
2020-08-24 23:00:54 +0530

    fixed an error in countdown (not show -ve value is event is over)

--- src/assets/js/countdown.js
+++ src/assets/js/countdown.js
@@ -6,14 +6,20 @@ let dt = localStorage.getItem('EventDate');
 
 let countDown = new Date(dt).getTime(),
     x = setInterval(function() {    
-
       let now = new Date().getTime(),
           distance = countDown - now;
-
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+         if(distance > 0 ) {
+          document.getElementById('days').innerText = Math.floor(distance / (day)),
+          document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+         }
+         else {
+          document.getElementById('days').innerText = "over",
+          document.getElementById('hours').innerText = "over",
+          document.getElementById('minutes').innerText = "over",
+          document.getElementById('seconds').innerText = "over";
+         }
 
       //do something later when date is reached
       //if (distance < 0) {
