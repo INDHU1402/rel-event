989e59ea3fb9dd6a35c80cd8b41a061cbd0eef1d
Alekhya77 <18wh1a1249@bvrithyderabad.edu.in>
2020-09-02 17:12:40 +0530

    update prof-cart

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
@@ -8,6 +8,93 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-event.component.css']
 })
 export class CreateEventComponent implements OnInit {
+  professionals: any;
+  chosenProfessional = [];
+  term: string;
+  name: string;
+
+  selected_games: { name: string; id: number; selected: boolean; }[];
+
+  searchText:string = "";
+  selected_count:number = 0;
+
+  
+  // Data Object to List Games
+  games = [
+    {
+      name:'photographer',
+      id:1,
+      selected:false
+    },
+    {
+      name:'decorator',
+      id:2,
+      selected:false
+    },
+    {
+      name:'caterer',
+      id:3,
+      selected:false
+    },
+    {
+      name:'Florist',
+      id:4,
+      selected:false},
+    {
+      name:'Dancer',
+      id:5,
+      selected:false
+    },
+    {
+      name:'Singer',
+      id:6,
+      selected:false},
+    {
+      name:'Technician',
+      id:7,
+      selected:false
+    }
+  ]
+  
+  
+  // Getting Selected Games and Count
+  getSelected(){
+     this.selected_games =  this.games.filter( s => {
+          return s.selected;
+        });
+     this.selected_count = this.selected_games.length;    
+     //alert(this.selected_games);    
+  }
+  
+  // Clearing All Selections
+  clearSelection(){
+    this.searchText = "";
+    this.games =  this.games.filter( g => {
+          g.selected = false;
+          return true;
+        });
+    this.getSelected();    
+  }
+  
+  //Delete Single Listed Game
+  deleteGame(id:number){
+    this.searchText = "";
+    this.games =  this.games.filter( g => {
+          if(g.id == id)
+          g.selected = false;
+          
+          return true;
+        });
+    this.getSelected(); 
+  }
+  
+  clearFilter(){
+    this.searchText = "";
+  }
+  
+
+
+
   myFilter1 = (d: Date | null): boolean => {
 
     this.date1 = Number(new Date().toDateString().slice(8,-5));
@@ -50,13 +137,12 @@ export class CreateEventComponent implements OnInit {
    reader:FileReader;
    imageUrl:String;
    Types: any = ['public', 'private']
-   professionals: any;
-   term:string;
+ 
    howManyDays:number;
    eventdate:Date;
    d:Date;
    Sponsor: any = ['yes', 'no'];
-   chosenProfessional = [];
+ 
    days = ['1','2','3','4','5','6','7','8','9'];
    Category: any = ['Show', 'Carnival', 'Fest', 'Sport Event', 'Plays', 'Charitable event', 'Art event'];
    eventDetails = {eventType:'', about:'', attendeesCount:'', category:'', eventName:'',guest1:'',guest2:'',guest3:'',
@@ -67,12 +153,15 @@ export class CreateEventComponent implements OnInit {
    User: any;
    constructor(private service: UserService, private router: Router) {
      this.imageUrl = 'src/assets/img/birthday.jpg';
+     this.name = `Angular! v${VERSION.full}`;
+  this.getSelected();
     }
  
    ngOnInit(): void {
      this.User = JSON.parse(localStorage.getItem('userDetails'));
      console.log(this.User);
      this.service.getProfessionalList().subscribe((result: any) => { console.log(result); this.professionals = result} );
+     
    }
    addProfessional(prof : any) : void{
      this.chosenProfessional.push(prof);
@@ -121,4 +210,19 @@ export class CreateEventComponent implements OnInit {
        this.imageUrl= event.target.result;
      };
    }
+
+   
+ 
+   done() {
+     console.log(this.chosenProfessional);
+     localStorage.setItem('chosenProf', JSON.stringify(this.chosenProfessional));
+     this.router.navigate(['techform']);
+   }
+
+   status() {
+    if ((this.selected_games.length) > 0) {
+      return true;
+    } 
+    return false;
+  }
 }
--- src/app/profcart/profcart.component.ts
+++ src/app/profcart/profcart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 
@@ -8,11 +8,102 @@ import { Router } from '@angular/router';
   styleUrls: ['./profcart.component.css']
 })
 export class ProfcartComponent implements OnInit {
+
+  
   professionals: any;
   chosenProfessional = [];
-
-  constructor(private service: UserService, private router: Router) { }
   term: string;
+  name: string;
+
+  selected_games: { name: string; id: number; selected: boolean; }[];
+
+  searchText:string = "";
+  selected_count:number = 0;
+
+  
+  // Data Object to List Games
+  games = [
+    {
+      name:'photographer',
+      id:1,
+      selected:false
+    },
+    {
+      name:'decorator',
+      id:2,
+      selected:false
+    },
+    {
+      name:'caterer',
+      id:3,
+      selected:false
+    },
+    {
+      name:'Florist',
+      id:4,
+      selected:false},
+    {
+      name:'Dancer',
+      id:5,
+      selected:false
+    },
+    {
+      name:'Singer',
+      id:6,
+      selected:false},
+    {
+      name:'Technician',
+      id:7,
+      selected:false
+    }
+  ]
+  
+  
+  // Getting Selected Games and Count
+  getSelected(){
+     this.selected_games =  this.games.filter( s => {
+          return s.selected;
+        });
+     this.selected_count = this.selected_games.length;    
+     //alert(this.selected_games);    
+  }
+  
+  // Clearing All Selections
+  clearSelection(){
+    this.searchText = "";
+    this.games =  this.games.filter( g => {
+          g.selected = false;
+          return true;
+        });
+    this.getSelected();    
+  }
+  
+  //Delete Single Listed Game
+  deleteGame(id:number){
+    this.searchText = "";
+    this.games =  this.games.filter( g => {
+          if(g.id == id)
+          g.selected = false;
+          
+          return true;
+        });
+    this.getSelected(); 
+  }
+  
+  clearFilter(){
+    this.searchText = "";
+  }
+  
+
+
+
+  constructor(private service: UserService, private router: Router) {
+
+  this.name = `Angular! v${VERSION.full}`;
+  this.getSelected();
+
+   }
+ 
 
   ngOnInit(): void {
     this.service.getProfessionalList().subscribe((result: any) => { console.log(result); this.professionals = result} );
@@ -36,4 +127,10 @@ export class ProfcartComponent implements OnInit {
      console.log(prof + "removed");
    }
 
+   status() {
+    if ((this.selected_games.length) > 0) {
+      return true;
+    } 
+    return false;
+  }
 }
