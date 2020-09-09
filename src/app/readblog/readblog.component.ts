b470cc10c48c674a6dbec58366722cba484c2cac
IndhuShivani <18wh1a0446@bvrithyderabad.edu.in>
2020-09-10 00:01:55 +0530

    added blog page UI, fixed error in countdown and spacing in header

--- src/app/readblog/readblog.component.ts
+++ src/app/readblog/readblog.component.ts
@@ -7,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadblogComponent implements OnInit {
   blog : any;
+  status: boolean;
+  status1: boolean;
   constructor() { }
 
   ngOnInit(): void {
     this.blog = JSON.parse(localStorage.getItem('Blog'));
     console.log(this.blog);
+    if(this.blog.profblog == null) {
+      this.status = false;
+      this.status1 = true;
+      console.log('is prof null = ' + this.status);
+    }
+    if(this.blog.userblog == null) {
+      this.status1 = false;
+      this.status = true;
+      console.log('is user null = ' + this.status1);
+
+    }
   }
 
 }
