d99bb78f24cb1a4763fd927ef29b1c027386b728
IndhuShivani <18wh1a0446@bvrithyderabad.edu.in>
2020-09-23 22:16:56 +0530

    changed professional portfolio UI

--- src/app/professional/professional.component.ts
+++ src/app/professional/professional.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-professional',
@@ -7,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessionalComponent implements OnInit {
   prof: any;
+  myEvents: any;
+  status: boolean;
 
-  constructor() { }
+  constructor(private service: UserService,private router: Router) { }
 
   ngOnInit(): void {
     this.prof = JSON.parse(localStorage.getItem('prof'));
+    if (this.prof.professionalName == null) {
+      this.status = false;
+    }
+    else {
+      this.status = true;
+    }
+    this.service.getMyEvents(this.prof.professionalId).subscribe((result: any) => {
+      console.log(result);this.myEvents= result });
+  }
+
+  goToEvent(event: any): void {
+    localStorage.setItem('eventDetails', JSON.stringify(event));
+    this.router.navigate(['showEvent']);
   }
  
 
