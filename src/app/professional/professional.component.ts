f8f0613a493e60ef2e6952af4614d73f1777aed2
IndhuShivani <18wh1a0446@bvrithyderabad.edu.in>
2020-09-24 21:54:48 +0530

    added professional component

---
+++ src/app/professional/professional.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-professional',
+  templateUrl: './professional.component.html',
+  styleUrls: ['./professional.component.css']
+})
+export class ProfessionalComponent implements OnInit {
+  prof: any;
+  myEvents: any;
+  status: boolean;
+  value: any;
+
+  constructor(private service: UserService,private router: Router) { }
+
+  ngOnInit(): void {
+    this.prof = JSON.parse(localStorage.getItem('prof'));
+    if (this.prof.professionalName == null) {
+      this.status = false;
+    }
+    else {
+      this.status = true;
+    }
+    this.service.getMyEvents(this.prof.professionalId).subscribe((result: any) => {
+      console.log(result);this.myEvents= result });
+
+    this.service.myrating(this.prof.professionalId).subscribe((op: any) => {
+        this.value = op;
+      });
+  }
+
+  goToEvent(event: any): void {
+    localStorage.setItem('eventDetails', JSON.stringify(event));
+    this.router.navigate(['showEvent']);
+  }
+ 
+
+}
